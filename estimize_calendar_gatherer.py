afb57afc0689a7c63bc5436d5e9575af9f3b1ec3
mobone <nicholas.reichel@gmail.com>
2020-01-14 19:10:56 -0600

    Optimizations

--- estimize_calendar_gatherer.py
+++ estimize_calendar_gatherer.py
@@ -17,7 +17,7 @@ from time import sleep
 from random import shuffle
 import sys
 import numpy as np
-
+import time
 class calendar(Process):
     def __init__(self, date_queue, lock, production, process_num):
         Process.__init__(self)
@@ -39,16 +39,17 @@ class calendar(Process):
         self.cur = self.conn.cursor()
         while self.date_queue.qsize()>0:
             self.start_date, self.end_date = self.date_queue.get(timeout=3)
-            total_eps_df = self.get_estimize_data('EPS')
-            total_revenue_df = self.get_estimize_data('Revenue')
+            self.get_estimize_data('EPS')
+            self.get_estimize_data('Revenue')
 
-            df = self.get_combined_df(total_eps_df, total_revenue_df)
-            print(df)
+            df = self.get_combined_df()
+            print('Output df', df)
             # store in the database
-            df.to_sql('estimize_data', self.conn, if_exists='append', index=False)
+            df.to_sql('estimize_data', self.conn, if_exists='append', index=True)
 
 
     def get_estimize_data(self, announcement_type):
+        print('Process Num', self.process_num, 'starting to gather', announcement_type, self.start_date.strftime('%Y-%m-%d'))
 
         # request the estimize website for data
         if announcement_type == 'EPS':
@@ -56,9 +57,10 @@ class calendar(Process):
         elif announcement_type == 'Revenue':
             url = 'https://www.estimize.com/calendar?tab=equity&metric=revenue&startDate=%s&endDate=%s' % (self.start_date.strftime('%Y-%m-%d'), self.end_date.strftime('%Y-%m-%d'))
         self.driver.get(url)
-        output_df = []
+
         previous_first_ticker = ''
         while True:
+            self.start_time = time.time()
             # check if there are no companies reporting earnings
             WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.CLASS_NAME , 'dAViVi')))
             companies_reporting_div = self.driver.find_element_by_class_name('dAViVi')
@@ -80,15 +82,21 @@ class calendar(Process):
             df = df.iloc[:, [2,3,5,6,7,8,9,10,12]]
             df.columns = ['Date Reported', 'Num of Estimates', 'Delta', 'Surprise', 'Historical Beat Rate', 'Wall St', 'Estimize', 'Actual', 'Symbol']
 
-            output_df.append(df)
-            df_length = len(pd.concat(output_df))/250
-            print('Process Number:', self.process_num, 'Progress:', round(df_length/80, 2))
-            if len(pd.concat(output_df))>260 and self.testing:
-                return output_df
+            date_reported_df = df['Date Reported'].str.split(' ', n = 1, expand = True)
+            date_reported_df = date_reported_df.rename(columns={0:"Date Reported", 1:"Time Reported"})
+            date_reported_df['Date Reported'] = pd.to_datetime(date_reported_df['Date Reported'])
 
+            df['Date Reported'] = date_reported_df['Date Reported']
+            df['Time Reported'] = date_reported_df['Time Reported']
+
+            df.to_sql('estimize_%s' % announcement_type, self.conn, if_exists='append', index=False)
+            if self.testing == True:
+                break
             try:
                 WebDriverWait(self.driver, self.delay).until(EC.presence_of_element_located((By.CLASS_NAME , 'eicEMB')))
                 elements = self.driver.find_elements_by_class_name('eicEMB')
+                if self.process_num == 1:
+                    self.get_progress(elements)
                 for element in elements:
                     if element.text == 'Next':
                         self.driver.execute_script("arguments[0].click();", element)
@@ -97,26 +105,25 @@ class calendar(Process):
                 print(e)
                 break
 
-        return output_df
+    def get_progress(self, elements):
+        try:
+            page_num = int(elements[2].text)-3
+            if page_num>0:
+                print('Complete:', round(page_num/80.0,2), 'Page Num:', page_num, 'Seconds:', round(time.time()-self.start_time , 1))
+        except Exception as e:
+            print('meow', e)
+            pass
 
-    def get_combined_df(self, total_eps_df, total_revenue_df):
-        eps_df = pd.concat(total_eps_df)
-        revenue_df = pd.concat(total_eps_df)
+
+    def get_combined_df(self):
+        year = self.start_date.strftime('%y')
+        print(self.start_date, self.end_date)
+        eps_df = pd.read_sql('select * from estimize_EPS where "Date Reported" < "%s" and "Date Reported" > "%s"' % (self.end_date.strftime('%Y-%m-%d'), self.start_date.strftime('%Y-%m-%d')), self.conn)
+        revenue_df = pd.read_sql('select * from estimize_Revenue where "Date Reported" < "%s" and "Date Reported" > "%s"' % (self.end_date.strftime('%Y-%m-%d'), self.start_date.strftime('%Y-%m-%d')), self.conn)
+
 
         del eps_df['Historical Beat Rate']
         del revenue_df['Historical Beat Rate']
-
-        date_reported_df = eps_df['Date Reported'].str.split(' ', n = 1, expand = True)
-        date_reported_df = date_reported_df.rename(columns={0:"Date Reported", 1:"Time Reported"})
-        date_reported_df['Date Reported'] = pd.to_datetime(date_reported_df['Date Reported'])
-        eps_df['Date Reported'] = date_reported_df['Date Reported']
-        eps_df['Time Reported'] = date_reported_df['Time Reported']
-
-        date_reported_df = revenue_df['Date Reported'].str.split(' ', n = 1, expand = True)
-        date_reported_df = date_reported_df.rename(columns={0:"Date Reported", 1:"Time Reported"})
-        date_reported_df['Date Reported'] = pd.to_datetime(date_reported_df['Date Reported'])
-        revenue_df['Date Reported'] = date_reported_df['Date Reported']
-        revenue_df['Time Reported'] = date_reported_df['Time Reported']
 
         eps_df = eps_df.sort_values(by='Date Reported')
         revenue_df = revenue_df.sort_values(by='Date Reported')
@@ -154,6 +161,18 @@ class calendar(Process):
 
         return tickers
 
+def drop_table(table_name):
+    conn = sqlite3.connect('earnings.db', timeout=120)
+    cur = conn.cursor()
+    try:
+        query = 'drop table estimize_%s' % table_name
+        cur.execute(query)
+        print('Dropped table', table_name)
+    except Exception as e:
+        print('Failed to drop table', table_name)
+        pass
+
+
 if __name__ == '__main__':
     if 'production' in sys.argv:
         production = True
@@ -165,13 +184,9 @@ if __name__ == '__main__':
             print('Exiting')
             exit()
         else:
-            try:
-                conn = sqlite3.connect('earnings.db', timeout=120)
-                cur = conn.cursor()
-                query = 'drop table estimize_data'
-                cur.execute(query)
-            except:
-                pass
+            drop_table('EPS')
+            drop_table('Revenue')
+            drop_table('data')
 
     lock = Lock()
 
@@ -182,7 +197,7 @@ if __name__ == '__main__':
     if production == True:
         date_list.append((datetime.now(), datetime.now()))
     else:
-        date_list.append((datetime.strptime('2013-01-01', '%Y-%m-%d'), datetime.strptime('2014-12-01', '%Y-%m-%d')))
+        date_list.append((datetime.strptime('2013-01-01', '%Y-%m-%d'), datetime.strptime('2013-12-01', '%Y-%m-%d')))
         date_list.append((datetime.strptime('2014-01-01', '%Y-%m-%d'), datetime.strptime('2014-12-31', '%Y-%m-%d')))
         date_list.append((datetime.strptime('2015-01-01', '%Y-%m-%d'), datetime.strptime('2015-12-31', '%Y-%m-%d')))
         date_list.append((datetime.strptime('2016-01-01', '%Y-%m-%d'), datetime.strptime('2016-12-31', '%Y-%m-%d')))
@@ -204,11 +219,11 @@ if __name__ == '__main__':
     for i in range(num_processes):
         p = calendar(date_queue, lock, production, i+1)
         p.start()
-        sleep(30)
+        sleep(5)
 
     while date_queue.qsize()>0 or processes_running:
         sleep(15)
-        print(date_queue.qsize())
+        #print(date_queue.qsize())
 
         if not any(process.is_alive() for process in processes):
             break
---
+++ progress_bar.py
@@ -0,0 +1,28 @@
+class ProgressBar:
+    def __init__(self, iterations, text='completed'):
+        self.text = text
+        self.iterations = iterations
+        self.prog_bar = '[]'
+        self.fill_char = '*'
+        self.width = 50
+        self.__update_amount(0)
+        self.elapsed = 1
+
+    def completed(self):
+        if self.elapsed > self.iterations:
+            self.elapsed = self.iterations
+        self.update_iteration(1)
+        print('\r' + str(self), end='')
+        _sys.stdout.flush()
+        print()
+
+    def animate(self, iteration=None):
+        if iteration is None:
+            self.elapsed += 1
+            iteration = self.elapsed
+        else:
+            self.elapsed += iteration
+
+        print('\r' + str(self), end='')
+        _sys.stdout.flush()
+        self.update_iteration()
